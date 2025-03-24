f3cb4cf10b035b7bbb4b0a3cd7b46ac3a1832001
Sahil Bhola <sbhola@umich.edu>
2025-03-23 21:54:20 -0400

    wip(tiledMatMult): Add boiler plate tiled mat mult

--- matMult/matMult.cu
+++ matMult/matMult.cu
@@ -1,8 +1,14 @@
 // Perform matrix-matrix multiplicatin of A \times B
 #include <cuda.h>
 
+#include <cassert>
 #include <iostream>
 
+const int M = 16;  // Number of rows of A
+const int N = 16;  // Number of cols of A
+const int T = 16;  // Number of cols of B
+
+#define TILE_SIZE 16  // Tile size for tiledMatMult
 #define cudaCheck(ans) gpuAssert((ans), __FILE__, __LINE__);
 
 inline void gpuAssert(cudaError_t err, const char *file, int line) {
@@ -31,25 +37,76 @@ __global__ void vanillaMatMultKernel(const int m, const int n, const int t,
   }
 }
 
+__global__ void tiledMatMultKernel(const int m, const int n, const int t,
+                                   float *A, float *B, float *C) {
+  // Initialize __shared__ memory
+  __shared__ float tile_A[TILE_SIZE * TILE_SIZE];
+  __shared__ float tile_B[TILE_SIZE * TILE_SIZE];
+
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+  int bx = blockIdx.x;
+  int by = blockIdx.y;
+
+  int row = by * TILE_SIZE + ty;  // Row of C that is evaluated the thread
+  int col = bx * TILE_SIZE + tx;  // Col of C that is evaluated the thread
+
+  float dot_product = 0.0f;
+  // Loop over the blocks
+  for (int bid = 0; bid < (n / TILE_SIZE); bid++) {
+    // Load the data from A and B to __shared __
+    // Each thread EXACTLY maps one value from A and B. A tile of the size
+    // blockDim is loaded. Say [C_1, C_2, C_3, C_4] is the block, then threads
+    // loads [A_1,..,A_4]
+
+    // row * N takes to the start fo the row
+    // bid * TILE_SIZE takes to the start of the block
+    // tx takes to the corresponding value
+    int idx_A = row * N + bid * TILE_SIZE + tx;
+
+    // (bid * TILE_SIZE + ty) * T: Skips all the rows and takes to the start
+    // col: offsets the column value
+    int idx_B = (bid * TILE_SIZE + ty) * T + col;
+
+    // Linear Indexing for the tile
+    int idx_tile = ty * TILE_SIZE + tx;
+
+    tile_A[idx_tile] = A[idx_A];  // Copy from DRAM to __shared__
+    tile_B[idx_tile] = B[idx_B];  // Copy from DRAM to __shared__
+
+    __syncthreads();  // All threads in block must have copied their data
+
+    for (int k = 0; k < TILE_SIZE; k++) {
+      dot_product += tile_A[ty * TILE_SIZE + tx] * tile_B[k * TILE_SIZE + col];
+    }
+
+    __syncthreads();  // All threads must be done using __shared__ data
+
+    C[row * t + col] = dot_product;
+  }
+}
+
+void initializeMatrix(float *A, float *B) {
+  // Allocate the host data
+  for (int i = 0; i < M * N; i++) {
+    A[i] = 1.0f;
+  }
+  for (int i = 0; i < N * T; i++) {
+    B[i] = 2.0f;
+  }
+}
+
 void vanillaMatMult() {
-  const int m = 32;  // Number of rows of A
-  const int n = 60;  // Number of cols of A
-  const int t = 64;  // Number of cols of B
-  int size_A = m * n * sizeof(float);
-  int size_B = n * t * sizeof(float);
-  int size_C = m * t * sizeof(float);
-  float *h_A = new float[m * n];
-  float *h_B = new float[n * t];
-  float *h_C = new float[m * t];
+  int size_A = M * N * sizeof(float);
+  int size_B = N * T * sizeof(float);
+  int size_C = M * T * sizeof(float);
+  float *h_A = new float[M * N];
+  float *h_B = new float[N * T];
+  float *h_C = new float[M * T];
   float *d_A, *d_B, *d_C;
 
-  // Allocate the host data
-  for (int i = 0; i < m * n; i++) {
-    h_A[i] = 1.0f;
-  }
-  for (int i = 0; i < n * t; i++) {
-    h_B[i] = 2.0f;
-  }
+  // Intialize
+  initializeMatrix(h_A, h_B);
 
   // Allocate the device memory
   cudaCheck(cudaMalloc((void **)&d_A, size_A));
@@ -60,10 +117,10 @@ void vanillaMatMult() {
 
   // Kernel parameters
   dim3 blockDim(16, 16, 1);  // Each thread would compute one element of output
-  dim3 gridDim(getGridDim(t, blockDim.x), getGridDim(m, blockDim.y), 1);
+  dim3 gridDim(getGridDim(T, blockDim.x), getGridDim(M, blockDim.y), 1);
 
   // Launch the kernel
-  vanillaMatMultKernel<<<gridDim, blockDim>>>(m, n, t, d_A, d_B, d_C);
+  vanillaMatMultKernel<<<gridDim, blockDim>>>(M, N, T, d_A, d_B, d_C);
   cudaCheck(cudaGetLastError());
 
   // Transfer data to host
@@ -71,8 +128,61 @@ void vanillaMatMult() {
 
   // Error check
   float error = 0.0f;
-  for (int i = 0; i < m * t; i++) {
-    error += (h_C[i] - n * 2.0);
+  for (int i = 0; i < M * T; i++) {
+    error += (h_C[i] - N * 2.0);
+  }
+  printf("Error: %f\n", error);
+
+  // Free
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
+}
+
+void tiledMatMult() {
+  // Tiled Matrix Multiplication
+  // Each thread loads data from the multiplicands to the __shared__
+  // Initialize
+  int size_A = M * N * sizeof(float);
+  int size_B = N * T * sizeof(float);
+  int size_C = M * T * sizeof(float);
+  float *h_A = new float[M * N];
+  float *h_B = new float[N * T];
+  float *h_C = new float[M * T];
+  float *d_A, *d_B, *d_C;
+
+  assert((M % TILE_SIZE == 0) &&
+         "Currently assumed that matrix align perfectly");
+  assert((N % TILE_SIZE == 0) &&
+         "Currently assumed that matrix align perfectly");
+  assert((T % TILE_SIZE == 0) &&
+         "Currently assumed that matrix align perfectly");
+
+  // Allocate Host
+  initializeMatrix(h_A, h_B);
+  // Allocate Device
+  cudaCheck(cudaMalloc((void **)&d_A, size_A));
+  cudaCheck(cudaMalloc((void **)&d_B, size_B));
+  cudaCheck(cudaMalloc((void **)&d_C, size_C));
+  // Copy Host to Device
+  cudaMemcpy(d_A, h_A, size_A, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_B, h_B, size_B, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_C, h_C, size_C, cudaMemcpyHostToDevice);
+  // Kernel paramters (BlockDim dosent necessarily have to be tile_size.)
+  dim3 blockDim(TILE_SIZE, TILE_SIZE, 1);
+  dim3 gridDim(getGridDim(T, blockDim.x), getGridDim(M, blockDim.y), 1);
+  // Kernel Launch
+  tiledMatMultKernel<<<gridDim, blockDim>>>(M, N, T, d_A, d_B, d_C);
+  cudaCheck(cudaGetLastError());
+
+  // Transfer Data
+  cudaCheck(cudaMemcpy(h_C, d_C, size_C, cudaMemcpyDeviceToHost));
+
+  // Error check
+  float error = 0.0f;
+  for (int i = 0; i < M * T; i++) {
+    std::cout << h_C[i] << std::endl;
+    error += (h_C[i] - N * 2.0);
   }
   printf("Error: %f\n", error);
 
@@ -84,7 +194,9 @@ void vanillaMatMult() {
 
 int main() {
   // Vanilla Kernel
-  vanillaMatMult();
+  /* vanillaMatMult(); */
+  // Tiled Kernel
+  tiledMatMult();
 
   return 0;
 }
